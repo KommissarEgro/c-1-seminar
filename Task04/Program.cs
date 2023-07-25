8edbad16fb7c6582c43cc1f62b2334a05250c787
Kommissar Egro <ugalovnik666@gmil.com>
2023-07-25 22:41:54 +1000

    Added Task06

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Введите первое целое число: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число: ");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+List<int>number = new List<int>{numberFirst, numberSecond, numberThird}(3);
+
+
