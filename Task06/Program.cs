fbc82bd548f6f3dbd767b45bb70bc903c3a4f19b
Kommissar Egro <ugalovnik666@gmil.com>
2023-07-25 23:00:19 +1000

    Решение задачи 6

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -8,8 +8,8 @@ Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0)
 {
-    Console.WriteLine("Число является четным");
+    Console.WriteLine("Да");
 }
 else {
-    Console.WriteLine();
+    Console.WriteLine("Нет");
 }
