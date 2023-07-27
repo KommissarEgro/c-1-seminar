e260316fd53319794ebbed9c8ee59ddab06e37b9
Kommissar Egro <ugalovnik666@gmil.com>
2023-07-27 12:55:49 +1000

    Решение 4 задачи

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -4,12 +4,22 @@
 // 22 3 9 -> 22
 
 
-Console.WriteLine("Введите первое целое число: ");
+Console.WriteLine("Напишите первое целое число: ");
 int numberFirst = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе целое число: ");
+Console.WriteLine("Напишиет второе целое число: ");
 int numberSecond = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье целое число: ");
-int numberThird = Convert.ToInt32(Console.ReadLine());
-List<int>number = new List<int>{numberFirst, numberSecond, numberThird}(3);
+Console.WriteLine("Напишите третье целое число: ");
+int numberThirt = Convert.ToInt32(Console.ReadLine());
 
+int max = numberFirst;
+
+if(numberFirst > max){
+    max = numberFirst;
+    if (numberSecond > max)
+    max = numberSecond;
+    if (numberThirt > max)
+    max = numberThirt; 
+}
+
+Console.WriteLine($"{max} максимальное число");
 
